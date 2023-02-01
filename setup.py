448e06719c87b089b9682d0dd927706326b6a9a6
Shalabh Chaturvedi <shalabh.chaturvedi+github@gmail.com>
2023-02-01 18:54:21 -0500

    Checkout dagster and build

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="test_proj",
     packages=find_packages(exclude=["test_proj_tests"]),
-    install_requires=["git+https://github.com/dagster-io/dagster@master", "dagster-cloud"],
+    install_requires=["dagster-cloud"],
     extras_require={"dev": ["dagit", "pytest"]},
 )
