c996aa0ee0e6b1d305db65137dda935bcd466ef2
Shalabh Chaturvedi <shalabh.chaturvedi+github@gmail.com>
2023-02-01 15:44:12 -0500

    different syntax

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="test_proj",
     packages=find_packages(exclude=["test_proj_tests"]),
-    install_requires=["git+https://github.com/dagster-io/dagster@release-1.1.15#egg=Dagster", "dagster-cloud"],
+    install_requires=["git+https://github.com/dagster-io/dagster@master", "dagster-cloud"],
     extras_require={"dev": ["dagit", "pytest"]},
 )
